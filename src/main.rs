23e71fa3e535d15f3df33883d4d0711a0989f47f
dashaw92 <dashaw92@gmail.com>
2022-10-23 11:15:03 -0400

    Finish 'A Minimal Rust Kernel'

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,18 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO: &[u8] = b"dashaw92 was here";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_ptr = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_ptr.offset(i as isize * 2) = byte;
+            *vga_ptr.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
